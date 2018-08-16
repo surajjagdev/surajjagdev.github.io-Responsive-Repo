4ececcd6c7b4e34c0a01ed9c7e4f3900b4155dca
Suraj Jagdev <100523120@oncampus.local>
2018-08-15 21:07:42 -0400

    customized the default superslides; added in fading pic feature, added auto change pic; falsified pagination

--- assets/javascript/superslides.js
+++ assets/javascript/superslides.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-  $("#slides").superslides();
+  $("#slides").superslides({
+    // documentation customizations
+    // //fading animation the animation based on the
+    animation: "fade",
+    //change automatically every 6 secs
+    play: "6000",
+    //get rid of default pagination
+    pagination: false
+  });
 });
