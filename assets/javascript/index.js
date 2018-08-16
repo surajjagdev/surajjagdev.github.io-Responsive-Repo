0826105ea330b9a25c0da9876b110e9a84246d54
Suraj Jagdev <100523120@oncampus.local>
2018-08-16 14:34:49 -0400

    completed the filtering portion, still need navbar, and optional icons display:none then on hover appear

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -41,4 +41,18 @@ $(document).ready(function() {
       queue: false
     }
   });
+  $("#filters a").click(function() {
+    $("#filters .current").removeClass("current");
+    $(this).addClass("current");
+    var selector = $(this).attr("data-filter");
+    $(".items").isotope({
+      filter: selector,
+      animationOptions: {
+        duration: 1500,
+        easing: "linear",
+        queue: false
+      }
+    });
+    return false;
+  });
 });
