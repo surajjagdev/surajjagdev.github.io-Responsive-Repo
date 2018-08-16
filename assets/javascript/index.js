b2c41546879df3e575d021bf98866513d5df9743
Suraj Jagdev <100523120@oncampus.local>
2018-08-16 13:08:46 -0400

    added in filters for isotope filter and fancy box images, all added in liri app pic and link and finished the portfolio links and pics section, still need to do filitering and then the nav bar

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -33,4 +33,12 @@ $(document).ready(function() {
     });
   }*/
   $("[data-fancybox]").fancybox();
+  $(".items").isotope({
+    filter: "*",
+    animationOptions: {
+      duration: 1500,
+      easing: "linear",
+      queue: false
+    }
+  });
 });
